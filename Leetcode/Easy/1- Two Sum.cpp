9430b46320ab1b8dd4bcb24f63cb0f8daab409c3
Tanush Krashak <139574093+TanushKrashak@users.noreply.github.com>
2024-02-25 14:44:56 +0530

    Create 1- Two Sum.cpp

---
+++ Leetcode/Easy/1- Two Sum.cpp
@@ -0,0 +1,28 @@
+// DIFFICULTY: Easy
+// Problem: 1. Two Sum
+// Problem Link: 
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+	for (int i = 0; i < nums.size() - 1; i++) {
+		for (int j = i + 1; j < nums.size(); j++) {
+			if (nums[i] + nums[j] == target)
+				return { i,j };
+		}
+	}
+	return {};
+}
+
+int main() {
+	vector<int> nums = { 2,11,7,15 };
+	int target = 9;
+	vector<int> result = twoSum(nums, target);
+	for (int i = 0; i < result.size(); i++) {
+		cout << result[i] << " ";
+	}
+	return 0;
+}
