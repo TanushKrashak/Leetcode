4bd862b150df0d9009655c5a3a3d881a1127eae7
Tanush Krashak <139574093+TanushKrashak@users.noreply.github.com>
2024-04-06 12:35:12 +0530

    Create 46- Permutations.cpp

---
+++ Cpp/Medium/46- Permutations.cpp
@@ -0,0 +1,65 @@
+
+// DIFFICULTY: Medium
+// Problem: 46. Permutations
+// Problem Link: https://leetcode.com/problems/permutations/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+void nextPermutation(vector<int>& nums, int size) {
+	int probIndex = size - 1;
+	if (size == 1)
+		return;
+	while (true) {
+		probIndex--;
+		if (nums[probIndex] < nums[probIndex + 1])
+			break;
+		if (probIndex <= 0)
+			break;
+	}
+recheck:
+	int justHighIndex = probIndex;
+	for (int i = probIndex + 1; i < size; i++) {
+		if (nums[probIndex] >= nums[justHighIndex]) {
+			justHighIndex = i;
+		}
+		if (nums[i] > nums[probIndex] && nums[i] <= nums[justHighIndex]) {
+			justHighIndex = i;
+		}
+	}
+	if (nums[justHighIndex] < nums[probIndex]) {
+		probIndex--;
+		if (probIndex < 0) {
+			sort(nums.begin(), nums.end());
+			return;
+		}
+		goto recheck;
+	}
+	else {
+		int temp = nums[justHighIndex];
+		nums[justHighIndex] = nums[probIndex];
+		nums[probIndex] = temp;
+		probIndex++;
+	}
+	sort(nums.begin() + probIndex, nums.end());
+	return;
+}
+
+vector<vector<int>> permute(vector<int>& nums) {
+	sort(nums.begin(), nums.end());
+	int size = nums.size();
+	vector<int> start = nums;
+	vector<vector<int>> ans;
+	nextPermutation(nums, size);
+	ans.push_back(nums);
+	while (start != nums) {
+		nextPermutation(nums, size);
+		ans.push_back(nums);
+	}
+	return ans;
+}
+
