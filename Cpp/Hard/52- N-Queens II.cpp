8b299326139c7923c77319ef964b73e770d909fe
Tanush Krashak <139574093+TanushKrashak@users.noreply.github.com>
2024-04-12 12:06:57 +0530

    Update 52- N-Queens II.cpp

--- Cpp/Hard/52- N-Queens II.cpp
+++ Cpp/Hard/52- N-Queens II.cpp
@@ -10,6 +10,7 @@
 
 using namespace std;
 
+// Fastest Solution
 int totalNQueens(int n) {
 	switch (n) {
 	case 1: return 1;
@@ -24,4 +25,42 @@ int totalNQueens(int n) {
 	} return 0;
 }
 
+// Actual Solution
+vector<int> upperLeftDiagnal;
+vector<int> upperRightDiagnal;
+vector<int> upperCol;
 
+int res = 0;
+vector<string> board;
+
+void helper(int row, int size) {
+	if (row == size) {
+		res++;
+		return;
+	}
+	for (int col = 0; col < size; col++) {
+		if (upperCol[col] == 0 and
+			upperLeftDiagnal[row + col] == 0 and
+			upperRightDiagnal[(size - 1) + col - row] == 0
+			) {
+			board[row][col] = 'Q';
+			upperCol[col] = upperLeftDiagnal[row + col] = upperRightDiagnal[(size - 1) + col - row] = 1;
+			helper(row + 1, size);
+			upperCol[col] = upperLeftDiagnal[row + col] = upperRightDiagnal[(size - 1) + col - row] = 0;
+			board[row][col] = '.';
+		}
+	}
+}
+
+int totalNQueens(int n) {
+	upperLeftDiagnal.resize(2*n-1, 0);
+	upperRightDiagnal.resize(2*n-1, 0);
+	upperCol.resize(n, 0);
+	board.resize(n);
+	string s(n, '.');
+	for (int i = 0; i < n; i++) {
+		board[i] = s;
+	}
+	helper(0, n);
+	return res;
+}
