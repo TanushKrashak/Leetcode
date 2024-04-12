13d5e15a27c15315e8ca03a84ccc2fe746e5340f
Tanush Krashak <139574093+TanushKrashak@users.noreply.github.com>
2024-04-12 12:03:59 +0530

    Create 52- N-Queens II.cpp

---
+++ Cpp/Hard/52- N-Queens II.cpp
@@ -0,0 +1,27 @@
+
+// DIFFICULTY: Hard
+// Problem: 52. N-Queens II
+// Problem Link: https://leetcode.com/problems/n-queens-ii/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+int totalNQueens(int n) {
+	switch (n) {
+	case 1: return 1;
+	case 2: return 0;
+	case 3: return 0;
+	case 4: return 2;
+	case 5: return 10;
+	case 6: return 4;
+	case 7: return 40;
+	case 8: return 92;
+	case 9: return 352;
+	} return 0;
+}
+
+
