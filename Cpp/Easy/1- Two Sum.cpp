5ec32c7680556e3315f2442c7c11b2b854a421e6
Tanush Krashak <139574093+TanushKrashak@users.noreply.github.com>
2024-04-17 11:49:27 +0530

    Update 1- Two Sum.cpp
    Comments

--- Cpp/Easy/1- Two Sum.cpp
+++ Cpp/Easy/1- Two Sum.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 vector<int> twoSum(vector<int>& nums, int target) {
 	int size = nums.size();
-	for (int i = 0; i < size - 1; i++) {
+	for (int i = 0; i < size - 1; i++) { // Loop Through Entire Array
 		for (int j = i + 1; j < size; j++) {
 			if (nums[i] + nums[j] == target)
 				return { i,j };
