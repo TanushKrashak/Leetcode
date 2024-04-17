ac359f7e2db282d22dcd65944efdb1f583750048
Tanush Krashak <139574093+TanushKrashak@users.noreply.github.com>
2024-04-17 11:42:08 +0530

    Update 1- Two Sum.cpp
    Improved Efficiency

--- Cpp/Easy/1- Two Sum.cpp
+++ Cpp/Easy/1- Two Sum.cpp
@@ -8,8 +8,9 @@
 using namespace std;
 
 vector<int> twoSum(vector<int>& nums, int target) {
-	for (int i = 0; i < nums.size() - 1; i++) {
-		for (int j = i + 1; j < nums.size(); j++) {
+	int size = nums.size();
+	for (int i = 0; i < size - 1; i++) {
+		for (int j = i + 1; j < size; j++) {
 			if (nums[i] + nums[j] == target)
 				return { i,j };
 		}
