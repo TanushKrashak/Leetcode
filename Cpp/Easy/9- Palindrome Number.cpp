ecb3ede35cad40206c113b2dda350d1f69c242e3
KonekoSupremacy <92970571+KonekoSupremacy@users.noreply.github.com>
2024-04-17 12:08:07 +0530

    Update and rename 9- Palindrome Number to 9- Palindrome Number.cpp

--- Cpp/Easy/9- Palindrome Number.cpp
+++ Cpp/Easy/9- Palindrome Number.cpp
@@ -9,11 +9,11 @@
 using namespace std;
 
 bool isPalindrome(int x) {
-	if (x < 0)
-		return false;
+	if (x < 0) // Handle Edge Case
+		return false; 
 	int temp = x;
 	int newX = 0;
-	while (temp > 0) {
+	while (temp > 0) {		
 		if ((long long int)(newX) * 10 + (temp % 10) >= pow(2, 31))
 			return false;
 		newX = newX * 10 + (temp % 10);
