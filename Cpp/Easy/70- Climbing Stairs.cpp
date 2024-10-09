22cb070724b87d438f1e197c52cf47c4980e9f20
Tanush Krashak <139574093+TanushKrashak@users.noreply.github.com>
2024-10-09 19:15:53 +0530

    Update 70- Climbing Stairs.cpp

--- Cpp/Easy/70- Climbing Stairs.cpp
+++ Cpp/Easy/70- Climbing Stairs.cpp
@@ -1,11 +1,25 @@
-  int climbStairs(int n) {
-      if (n==0) 
-          return 0;
-      int a =0, b=1, c = 1;
-      for (int i =0; i<n; i++) {
-          c = a + b;
-          a = b;
-          b = c;
-      }
-      return c;
-  }
+
+// DIFFICULTY: Easy	
+// Problem: 70. Climbing Stairs
+// Problem Link: https://leetcode.com/problems/climbing-stairs/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <string>
+
+using namespace std;
+
+
+int climbStairs(int n) {
+    if (n==0) 
+        return 0;
+    int a =0, b=1, c = 1;
+    for (int i =0; i<n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return c;
+}
