ad7d79414e8ac4960a475b377d6ecfb3cca91ece
Tanush Krashak <139574093+TanushKrashak@users.noreply.github.com>
2024-11-06 20:29:25 +0530

    Update 71- Simplify Path.cpp

--- Cpp/Medium/71- Simplify Path.cpp
+++ Cpp/Medium/71- Simplify Path.cpp
@@ -1,45 +1,51 @@
-class Solution {
-public:
-    string simplifyPath(string path) {
-        string ans = "";
-        int size = path.size();
-        string temp = "";
-        int ansSize = 0;
-        for (int i =0; i<size; i++) {
-            cout << ans << endl;
-            if (path[i] == '/' || i == size-1) {
-                if (i==size-1 && path[i] != '/') {
-                    temp += path[i];
-                }
-                if (temp == "") {                                      
-                    continue;
-                }
-                if (temp == ".") { 
-                    temp = "";
-                    continue;                   
-                }
-                if (temp == "..") {
-                    int counter = 0;
-                    while (ansSize > 1 && counter < 1) {
-                        counter += (ans[ansSize-1] == '/') ? 1:0;
-                        ans.erase(ansSize-1, 1);
-                        ansSize -= (counter==1) ? 0:1;
-                    }   
-                    temp = ""; 
-                    continue;
-                }    
-                ans += (ansSize > 0 && ans[ansSize-1] =='/') ? "":"/";
-                ansSize += (ansSize > 0 && ans[ansSize-1] =='/') ? 0:1;                             
-                ans += temp;                
-                ansSize += temp.size();
-                temp = "";
+// DIFFICULTY: Medium
+// Problem: 71. Simplify Path
+// Problem Link: https://leetcode.com/problems/simplify-path/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+string simplifyPath(string path) {
+    string ans = "";
+    int size = path.size();
+    string temp = "";
+    int ansSize = 0;
+    for (int i =0; i<size; i++) {
+        cout << ans << endl;
+        if (path[i] == '/' || i == size-1) {
+            if (i==size-1 && path[i] != '/') {
+                temp += path[i];
+            }
+            if (temp == "") {                                      
                 continue;
             }
-            temp += path[i];
+            if (temp == ".") { 
+                temp = "";
+                continue;                   
+            }
+            if (temp == "..") {
+                int counter = 0;
+                while (ansSize > 1 && counter < 1) {
+                    counter += (ans[ansSize-1] == '/') ? 1:0;
+                    ans.erase(ansSize-1, 1);
+                    ansSize -= (counter==1) ? 0:1;
+                }   
+                temp = ""; 
+                continue;
+            }    
+            ans += (ansSize > 0 && ans[ansSize-1] =='/') ? "":"/";
+            ansSize += (ansSize > 0 && ans[ansSize-1] =='/') ? 0:1;                             
+            ans += temp;                
+            ansSize += temp.size();
+            temp = "";
+            continue;
         }
-        if (ansSize > 1 && ans[ansSize-1] == '/')
-            ans.erase(ansSize-1, 1);
-        ans += (ansSize==0)? "/":"";
-        return ans;
+        temp += path[i];
     }
-};
+    if (ansSize > 1 && ans[ansSize-1] == '/')
+        ans.erase(ansSize-1, 1);
+    ans += (ansSize==0)? "/":"";
+    return ans;
+}
